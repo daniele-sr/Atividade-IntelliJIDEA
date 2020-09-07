c826bc184bdbae3a67ff276db9a046c242ae4e30
danielerochabandtec <daniele.rocha@bandtec.com.br>
2020-09-07 12:26:57 -0300

    criação do projeto

---
+++ atividade-individual/src/main/java/br/com/bandtec/atividadeindividual/AtividadeIndividualApplication.java
@@ -0,0 +1,13 @@
+package br.com.bandtec.atividadeindividual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeIndividualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeIndividualApplication.class, args);
+	}
+
+}
---
+++ atividade-individual/src/test/java/br/com/bandtec/atividadeindividual/AtividadeIndividualApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bandtec.atividadeindividual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeIndividualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
